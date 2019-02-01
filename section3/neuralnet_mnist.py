abaa3c970efbfe4a2523ee172404875e51d74af2
try46 <popuman61@gmail.com>
2019-02-01 21:19:41 +0900

    util.pyを実装

--- section3/neuralnet_mnist.py
+++ section3/neuralnet_mnist.py
@@ -5,4 +5,16 @@ sys.path.append(os.pardir)
 import numpy as np
 import pickle
 from dataset.mnist import load_mnist
-from common.funcitons import sigmoid, softmax
+from common.functions import sigmoid, softmax
+
+def get_data():
+    (x_train,t_train),(x_test,t_test) = load_mnist(normalize=True,flatten=True,one_hot_label=False)
+    return x_test,t_test
+
+def init_network():
+    with open("sample_weight.pkl",'rb') as f:
+        nerwork = pickle.load(f)
+    return nerwork
+
+def predict(network, x):
+    w1,w2.w3 = network['w1'],network['w2'],network['w3']
