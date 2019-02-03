f980c7fd1e42e62a256890197341bd170e605136
try46 <popuman61@gmail.com>
2019-02-04 02:56:51 +0900

    Relu,Sigmoidクラスを実装した

--- common/layers.py
+++ common/layers.py
@@ -2,5 +2,62 @@
 
 import numpy as np
 from common.functions import *
-from common.util import im2col,col2im
+from common.util import im2col, col2im
 
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = sigmoid(x)
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+
+        self.x = None
+        self.original_x_shape = None
+        self.dw = None
+        self.db = None
+
+    def forwad(self, x):
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+        out = np.dot(self.x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dw = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        dx = dx.reshape(*self.original_x_shape)
+        return dx
