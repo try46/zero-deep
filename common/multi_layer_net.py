e5d78bf93e713a568d649da4e288a852493c18bc
try46 <popuman61@gmail.com>
2019-04-29 01:41:23 +0900

    update common/

---
+++ common/multi_layer_net.py
@@ -0,0 +1,54 @@
+from common.gradient import numerical_gradient
+from common.layers import *
+from collections import OrderdDict
+import numpy as np
+import sys
+import os
+sys.path.append(os.pardir)
+
+
+class MultiLayerNet:
+    def __init__(self, input_size, hidden_size_list, output_size, activation='relu', weight_init_std='relu', weight_decay_lambda=0):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size_list = hidden_size_list
+        self.hidden_layer_num = len(hidden_size_list)
+        self.weight_decay_lambda = weight_decay_lambda
+        self.params = {}
+
+        self.__init_weight(weight_init_std)
+        activation_layer = {'sigmoid': Sigmoid, 'relu': Relu}
+
+        self.layers = OrderdDict()
+        for idx in range(1, self.hidden_layer_num+1):
+            self.layers['Affine'+str(idx)] = Affine(self.params['W'+str(idx)],
+                                                    self.params['b'+str(idx)])
+            self.layers['Activation_function' +
+                        str(idx)] = activation_layer[activation]()
+        idx = self.hidden_layer_num + 1
+        self.layers['Affine'+str(idx)] = Affine(self.params['W'+str(idx)],
+                                                self.params['b'+str(idx)])
+        self.last_layer = SoftmaxWithLoss()
+
+
+    def __init_weight(self,weight_init_std):
+        all_size_list = [self.input_size] + self.hidden_size_list + [self.output_size]
+        for idx in range(1,len(all_size_list)):
+            scale = weight_init_std
+            if str(weight_init_std).lower in ('relu','he'):
+                scale = np.sqrt(2.0/all_size_list[idx - 1])
+            elif str(weight_init_std).lower() in ('sigmoid','xavier'):
+                scale = n[.sqrt(1.0/all_size_list[idx - 1])]
+            
+            self.params['W'+str(idx)] = scale * np.random.randn(all_size_list[idx-1],all_size_list[idx])
+            self.params['b'+str(idx)] = np.zeros(all_size_list[idx])
+
+    def predict(self,f):
+        for i in self.layers.values():
+            x = layers.forward(x)
+
+        return x
+
+
+    def loss(self,x,y):
+
